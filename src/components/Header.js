075f40ad4a3c24c09be1de6ebb3527448788071c
Yohan Fleury <yfleury95@gmail.com>
2021-03-22 16:02:02 +0100

    header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,14 @@ import logo from 'assets/logo-officiel.png'
 
 const Header = () => {
     return (
-        <div className="container-fluid semi-header text-white mt-0 parent">
-            <div className="container text-center">
-                <div className="d-flex flex-column align-items-center">
-                    <img src={logo} alt="logo" className="img-fluid logo-size mt-2" />
-                    <p className="police"> Votre Partenaire qui vous accompagne dans vos projets de création d'aménagement et de réaménagement sur mesure. </p>
-                    <button className="btn btn-size mt-4"> Découvrez nos créations </button>
+        <div className="semi-header text-white">
+            <div className="container">
+                <div className="d-flex row justify-content-center align-items-center">
+                    <img src={logo} alt="logo" className="img-fluid logo-size mt-3" />
+                    <p className="police mt-3"> Votre Partenaire qui vous accompagne dans vos projets de création d'aménagement et de réaménagement sur mesure. </p>
+                    <button className="btn btn-size mt-4 mb-4"> Découvrez nos créations </button>     
                 </div>
             </div>
-            
         </div>
     )
 }
